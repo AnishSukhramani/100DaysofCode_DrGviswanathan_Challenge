644f7f81a517715e12aab3d27b1a1070184ee46e
Anish Sukhramani <91376836+AnishSukhramani@users.noreply.github.com>
2023-11-08 13:59:50 +0530

    Day - 10 : 8-11-2023 - 35. Search Insert Position.py

--- Day - 10 : 8-11-2023 - 35. Search Insert Position.py
+++ Day - 10 : 8-11-2023 - 35. Search Insert Position.py
@@ -9,3 +9,24 @@ class Solution:
             nums.append(target)
             nums.sort()
             return nums.index(target)
+
+# This Python code defines a class named `Solution` with a method `searchInsert`. The purpose of this method is to find the index at which a specified target integer should be inserted into a given sorted array of integers. If the target is already in the array, it should return the index of the target, and if the target is not in the array, it should return the index where the target would be inserted while maintaining the sorted order of the array.
+# Here's a breakdown of the code:
+
+# 1. `def searchInsert(self, nums: List[int], target: int) -> int:` - This line defines the `searchInsert` method, which takes two arguments:
+#    - `self` is a reference to the instance of the class (it's a typical convention in object-oriented programming).
+#    - `nums` is a list of integers that represents the sorted array in which we need to find the target.
+#    - `target` is an integer that represents the value we want to find or insert into the `nums` list.
+
+# 2. `if target in nums:` - This line checks if the `target` is already present in the `nums` list. If it is, it enters this block.
+
+#    - `return nums.index(target)` - If the `target` is found in the list, it returns the index of the `target` in the list using the `index` method. This index is the position at which the `target` is found in the sorted array.
+
+# 3. If the `target` is not found in the `nums` list, the code executes the `else` block:
+
+#    - `nums.append(target)` - It adds the `target` to the end of the `nums` list.
+#    - `nums.sort()` - It sorts the `nums` list to maintain the sorted order.
+
+# 4. Finally, `return nums.index(target)` is used to find and return the index at which the `target` is now located in the sorted list. Since we have inserted the `target` and sorted the list, the index returned is the position where the `target` would be inserted while keeping the list sorted.
+
+# So, this code effectively handles two cases: finding the index of an existing target or determining the index where the target would be inserted to maintain a sorted order in the array.
