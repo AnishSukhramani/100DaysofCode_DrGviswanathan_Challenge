9642238bee04f3e12a6590a959a207ec96bc1054
Anish Sukhramani <91376836+AnishSukhramani@users.noreply.github.com>
2023-11-09 19:39:03 +0530

    Day - 11 : 9-11-2023 - 50. Pow(x, n).py
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
    
     
    
    Example 1:
    
    Input: x = 2.00000, n = 10
    Output: 1024.00000
    Example 2:
    
    Input: x = 2.10000, n = 3
    Output: 9.26100
    Example 3:
    
    Input: x = 2.00000, n = -2
    Output: 0.25000
    Explanation: 2-2 = 1/22 = 1/4 = 0.25
     
    
    Constraints:
    
    -100.0 < x < 100.0
    -231 <= n <= 231-1
    n is an integer.
    Either x is not zero or n > 0.
    -104 <= xn <= 104

---
+++ Day - 11 : 9-11-2023 - 50. Pow(x, n).py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def myPow(self, x, n):
+        return x**n
+
+# This is a simple Python class with a method called `myPow`. It takes two parameters, `x` and `n`, and returns the result of raising `x` to the power of `n` using the `**` operator.
