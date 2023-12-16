3ed441b4abefc88ea2e11a936ded9011bf5eefc0
Anish Sukhramani <91376836+AnishSukhramani@users.noreply.github.com>
2023-12-16 23:32:32 +0530

    Day - 48 : 16-12-2023 - 2011. Final Value of Variable After Performing Operations.py
    There is a programming language with only four operations and one variable X:
    
    ++X and X++ increments the value of the variable X by 1.
    --X and X-- decrements the value of the variable X by 1.
    Initially, the value of X is 0.
    
    Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
    
     
    
    Example 1:
    
    Input: operations = ["--X","X++","X++"]
    Output: 1
    Explanation: The operations are performed as follows:
    Initially, X = 0.
    --X: X is decremented by 1, X =  0 - 1 = -1.
    X++: X is incremented by 1, X = -1 + 1 =  0.
    X++: X is incremented by 1, X =  0 + 1 =  1.
    Example 2:
    
    Input: operations = ["++X","++X","X++"]
    Output: 3
    Explanation: The operations are performed as follows:
    Initially, X = 0.
    ++X: X is incremented by 1, X = 0 + 1 = 1.
    ++X: X is incremented by 1, X = 1 + 1 = 2.
    X++: X is incremented by 1, X = 2 + 1 = 3.
    Example 3:
    
    Input: operations = ["X++","++X","--X","X--"]
    Output: 0
    Explanation: The operations are performed as follows:
    Initially, X = 0.
    X++: X is incremented by 1, X = 0 + 1 = 1.
    ++X: X is incremented by 1, X = 1 + 1 = 2.
    --X: X is decremented by 1, X = 2 - 1 = 1.
    X--: X is decremented by 1, X = 1 - 1 = 0.
     
    
    Constraints:
    
    1 <= operations.length <= 100
    operations[i] will be either "++X", "X++", "--X", or "X--".

---
+++ Day - 48 : 16-12-2023 - 2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        x=0
+        for i in range(len(operations)):
+            if operations[i]=="++X" or operations[i]=="X++":
+                x+=1
+            elif operations[i]=="--X" or operations[i]=="X--":
+                x-=1
+        return x
